e7ab77c70f8a1d6c4d56e520af7499cc40de86fa
kadu <kadu.silva2014@gmail.br>
2022-11-07 18:45:16 -0300

    Fix:🐞 run dev from backend running in docker

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
 import { Server } from "socket.io";
-import cors from 'cors';
-import http from 'http';
-import 'dotenv/config'
+import cors from "cors";
+import http from "http";
+import "dotenv/config";
 
 export type IMessagem = {
   socketId?: string;
@@ -10,51 +10,52 @@ export type IMessagem = {
   userName: string;
   text: string;
   createdAt: Date;
-}
+};
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-  }
+  },
 });
 
-const messagens: IMessagem[] = []
+const messagens: IMessagem[] = [];
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(cors({
-  origin: '*',
-  methods: '*',
-  credentials:true,
-}))
+app.use(
+  cors({
+    origin: "*",
+    methods: "*",
+    credentials: true,
+  })
+);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
-    message: 'Hello, world!'
+    message: "Hello, world!",
   });
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`a user connected: ${socket.id}`);
 
-  socket.on('sendMessage', (message) => {
-    console.log(`send - ${message.userName} | ${message.text}`)
-    messagens.push(message)
-    socket.broadcast.emit('reciveMessage', message);
-  })
+  socket.on("sendMessage", (message) => {
+    console.log(`send - ${message.userName} | ${message.text}`);
+    messagens.push(message);
+    socket.broadcast.emit("reciveMessage", message);
+  });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`a user disconnet: ${socket.id}`);
-  })
+  });
 
-  socket.on('getMessage', (data, callback) => {
-    callback(messagens)
-  })
+  socket.on("getMessage", (data, callback) => {
+    callback(messagens);
+  });
 });
 
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
 });
-
